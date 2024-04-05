cd2c22b718f39081e693826e8e707f43f4bc9ac3
Mauros721 <157764840+Mauros721@users.noreply.github.com>
2024-04-05 14:23:18 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const getPronounAdjectiveList = () => {
+  let state = [];
+  const pronounList = ["the", "our"];
+  const adjectiveList = ["big", "great"];
+  return pronounList.reduce((state, pronoun) => {
+    adjectiveList.forEach((adjective) => {
+      state = [...state, `${pronoun}${adjective}`];
+    });
+  return state;  
+}, []);
 };
+
+const getPronounAdjectiveNounList = (pronounAdjectiveList) => {
+  const nounList = ["jogger", "racoon"];
+  const domainList = [".com", ".net"];
+  return pronounAdjectiveList.reduce((state, pronounAdjective) => {
+    nounList.forEach((noun) => {
+      domainList.forEach((domain) => {
+        state = [...state, `${pronounAdjective}${noun}${domain}`];
+      });
+    });
+    return state;
+  }, []);
+};
+
+const printPronounAdjectiveNounList = (pronounAdjectiveList) => {
+  pronounAdjectiveList.forEach(pronounAdjectiveNoun => {
+    console.log(pronounAdjectiveNoun);
+  });
+};
+
+function main() {
+  const pronounAdjectiveList = getPronounAdjectiveList();
+  const pronounAdjectiveNounList = getPronounAdjectiveNounList(
+    pronounAdjectiveList
+  );
+  printPronounAdjectiveNounList(pronounAdjectiveNounList);
+}
+
+main();
